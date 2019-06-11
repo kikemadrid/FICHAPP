5cc29c21646bf03d18de56149143ad24b2d750da
kike <enriquem1985@gmail.com>
2019-06-11 22:28:17 +0200

    Implementación de los botones de registro, modificación y elminación.

--- app/src/main/java/com/example/fichapp/vistas/RegistroEmpresaActivity.java
+++ app/src/main/java/com/example/fichapp/vistas/RegistroEmpresaActivity.java
@@ -1,8 +1,11 @@
 package com.example.fichapp.vistas;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.fichapp.R;
@@ -29,9 +32,23 @@ public class RegistroEmpresaActivity extends AppCompatActivity {
         //Activamos el escuchador que al peder el foco relizará las posteriores validaciones por cada caja del formulario
         activarFocoValidacion();
 
+        // hay que determinar que vamos a recibir
+        String accion = getIntent().getStringExtra("accion");
+
+        logicaBotones(accion);
+
     }
 
-    public void activarFocoValidacion () {
+    public void logicaBotones(String accion) {
+        if (accion.equals("usuario")) {
+            Button botonM = (Button) findViewById(R.id.botonmodificar);
+            Button botonE = (Button) findViewById(R.id.botoneliminar);
+            botonM.setEnabled(false);
+            botonE.setEnabled(false);
+        }
+    }
+
+    public void activarFocoValidacion() {
 
         cajatextocif = (EditText) findViewById(R.id.cajacif);
         cajatextocif.setOnFocusChangeListener(new FocusListenerFormularios(this));
@@ -47,18 +64,36 @@ public class RegistroEmpresaActivity extends AppCompatActivity {
     }
 
 
-    public void enviar(View v) {
-
+    public void registrar(View v) {
 
 
     }
 
     public void modificar(View v) {
 
+
     }
 
     public void eliminar(View v) {
 
+        AlertDialog.Builder dialogo1 = new AlertDialog.Builder(this);
+        dialogo1.setTitle("Importante");
+        dialogo1.setMessage("Si elimina la empresa se eliminarán todos los empleados dados de alta. ¿Desea continuar?");
+        dialogo1.setCancelable(false);
+        dialogo1.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialogo1, int id) {
+                // Codigo que llamará a la eliminación de la empresa
+            }
+        });
+        dialogo1.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialogo1, int id) {
+                // Se cancela la operación de elmiminación
+            }
+        });
+        dialogo1.show();
     }
+
+
+
 
 }
